6f4e6fcf1ab82d10a24a04452ba86f9ae96b55d4
Marlo <adambuawnuk@gmail.com>
2020-07-05 23:59:24 +0200

    add dlq configuration

--- src/main/java/com/co/smart/app/listener/configuration/RabbitmqConfiguration.java
+++ src/main/java/com/co/smart/app/listener/configuration/RabbitmqConfiguration.java
@@ -6,22 +6,21 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
 public class RabbitmqConfiguration {
 
-    private final MeasurementAddedQueueConfiguration measurementAddedQueue;
+    private static final String SMART_DLQ = "smart.dlq";
+
+    private final MeasurementAddedQueueConfiguration measurementAddedQueueConfiguration;
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
@@ -42,22 +41,38 @@ public class RabbitmqConfiguration {
     }
 
     @Bean
-    TopicExchange dlqExchange() {
-        return new TopicExchange("smart.dlq");
+    FanoutExchange dlqExchange() {
+        return new FanoutExchange(SMART_DLQ);
     }
 
+    //endregion
+
+    //region queue config
+
+    @Bean
+    Queue dlqQueue(){
+        return new Queue(SMART_DLQ);
+    }
+
+    @Bean
+    Binding dlqBinding(Queue dlqQueue, TopicExchange dlqExchange) {
+        return BindingBuilder.bind(dlqQueue).to(dlqExchange).with(SMART_DLQ);
+    }
     //endregion
 
     //region Measurement added queue configuration
 
     @Bean
     Queue measurementAddedQueue() {
-        return new Queue(measurementAddedQueue.getQueue(), false);
+        Map<String, Object> args = new HashMap<String, Object>();
+        args.put("x-dead-letter-exchange", SMART_DLQ);
+        args.put("x-dead-letter-routing-key", SMART_DLQ);
+        return new Queue(measurementAddedQueueConfiguration.getQueue(), true, false,false, args);
     }
 
     @Bean
-    Binding measurementAddedBinding(Queue queue, TopicExchange devicesExchange) {
-        return BindingBuilder.bind(queue).to(devicesExchange).with(measurementAddedQueue.getRoutingKey());
+    Binding measurementAddedBinding(Queue measurementAddedQueue, TopicExchange devicesExchange) {
+        return BindingBuilder.bind(measurementAddedQueue).to(devicesExchange).with(measurementAddedQueueConfiguration.getRoutingKey());
     }
 
     //endregion
